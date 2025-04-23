62912042d920086ff1be26a0c520bbd70307d5a8
KajalGhule <kajalghule111@gmail.com>
2025-04-23 11:23:08 +0530

    modifications for new future

--- TiffinManagementSystem/src/main/java/com/annapurna/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.annapurna.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.annapurna.pojos.User;
-
-public interface UserDao extends JpaRepository<User, Integer>{
-
-}
